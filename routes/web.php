a2e0aeba32f504d7af352972cb925ff50b257692
Muhammad Bunyamin <bunyamin250391@gmail.com>
2020-03-11 18:55:29 +0700

    contoh route paramenter required

--- routes/web.php
+++ routes/web.php
@@ -10,26 +10,32 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-$router->get('/user', function () use ($router) {
+$router->get('/user', function () {
     return 'get user';
 });
 
-$router->post('/user', function () use ($router) {
+$router->post('/user', function () {
     return 'post user';
 });
 
-$router->put('/user', function () use ($router) {
+$router->put('/user', function ()  {
     return 'put user';
 });
 
-$router->delete('/user', function () use ($router) {
+$router->delete('/user', function () {
     return 'delete user';
 });
 
-$router->patch('/user', function () use ($router) {
+$router->patch('/user', function () {
     return 'patch user';
 });
 
 $router->options('/user', function () use ($router) {
     return 'options user';
+});
+
+//contoh route parameter required
+$router->get('/user/{id}/{name}/{nim}', function ($id,$a,$b) {
+    $c = $a+$b;
+    return 'get user dengan id '.$id.' hasil tambah '.$c;
 });
